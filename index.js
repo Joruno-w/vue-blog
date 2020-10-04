c392b3cdfa4c0b9ae9b3638faf93b3978975fa0e
wangshengliang <1710642275@qq.com>
2020-10-04 20:45:29 +0800

    right-module

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
-require('./models/init');
 const app = express();
-app.use(express.static('./page/'));
+app.use('/',express.static('./page/'));
 app.listen(12306,()=>{
     console.log('服务器已启动！');
 });
