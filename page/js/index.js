eac4a33ffc6cc7b979a8ed3ee8582224b39c2abe
wangshengliang <1710642275@qq.com>
2020-10-05 11:24:36 +0800

    vue articleList

---
+++ page/js/index.js
@@ -0,0 +1,53 @@
+const vm1 = new Vue({
+    el: '#every_day',
+    data: {
+        content: 'An animal\'s eyes have the power to speak a great language.'
+    },
+    created(){
+        //访问接口
+    }
+});
+
+const vm2 = new Vue({
+    el: '#articleList',
+    data: {
+        articleList: [
+            {
+                title: '在Nginx中将http://zh30.com:443跳转到https://zh30.com:443',
+                link: '',
+                content: '有小伙伴反应我博客半年没更新了，借此机会赶紧水一篇。另有小伙伴求助于我一个这样的问题，说在使用http://协议外加443端口访问时，nginx会报错提示：“400 Bad Request The plain HTTP request was sent to HTTPS port”这个错误是指请求错误，http协议的请求被发送到了https的端口。在Nginx中，不能在一个端口同时处理http和https请求。按正常浏览来说也不可能会...',
+                publishDate: '2020-05-22',
+                scan: '1,097',
+                tags: 'bad request error_page nginx'
+            },
+            {
+                title: '在Nginx中将http://zh30.com:443跳转到https://zh30.com:443',
+                link: '',
+                content: '有小伙伴反应我博客半年没更新了，借此机会赶紧水一篇。另有小伙伴求助于我一个这样的问题，说在使用http://协议外加443端口访问时，nginx会报错提示：“400 Bad Request The plain HTTP request was sent to HTTPS port”这个错误是指请求错误，http协议的请求被发送到了https的端口。在Nginx中，不能在一个端口同时处理http和https请求。按正常浏览来说也不可能会...',
+                publishDate: '2020-05-22',
+                scan: '1,097',
+                tags: 'bad request error_page nginx'
+            },
+            {
+                title: '在Nginx中将http://zh30.com:443跳转到https://zh30.com:443',
+                link: '',
+                content: '有小伙伴反应我博客半年没更新了，借此机会赶紧水一篇。另有小伙伴求助于我一个这样的问题，说在使用http://协议外加443端口访问时，nginx会报错提示：“400 Bad Request The plain HTTP request was sent to HTTPS port”这个错误是指请求错误，http协议的请求被发送到了https的端口。在Nginx中，不能在一个端口同时处理http和https请求。按正常浏览来说也不可能会...',
+                publishDate: '2020-05-22',
+                scan: '1,097',
+                tags: 'bad request error_page nginx'
+            },
+            {
+                title: '在Nginx中将http://zh30.com:443跳转到https://zh30.com:443',
+                link: '',
+                content: '有小伙伴反应我博客半年没更新了，借此机会赶紧水一篇。另有小伙伴求助于我一个这样的问题，说在使用http://协议外加443端口访问时，nginx会报错提示：“400 Bad Request The plain HTTP request was sent to HTTPS port”这个错误是指请求错误，http协议的请求被发送到了https的端口。在Nginx中，不能在一个端口同时处理http和https请求。按正常浏览来说也不可能会...',
+                publishDate: '2020-05-22',
+                scan: '1,097',
+                tags: 'bad request error_page nginx'
+            }
+        ]
+    },
+    created(){
+
+    }
+})
+
